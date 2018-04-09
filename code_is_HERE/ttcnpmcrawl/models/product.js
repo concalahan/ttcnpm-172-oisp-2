18c503f25aa8b8550720ec8ef6ea9f37ed295316
= <mainguyenvu210@gmai.com>
2018-04-09 22:00:45 +0700

    can crawl existing product

--- code_is_HERE/ttcnpmcrawl/models/product.js
+++ code_is_HERE/ttcnpmcrawl/models/product.js
@@ -1,22 +1,25 @@
 var mongoose = require("mongoose");
 
 var ProductSchema = new mongoose.Schema({
-    productId: String,
+    product_id: String,
     name: String,
     url_path: String,
+    thumbnail_url: String,
+    more_thumbnail_url: [ String ],
     price: [{
         value: Number,
         date: {type: Date, default: Date.now}
     }],
     provider: String,
-    categoryType: String,
-    image_url: String,
+    category_type: String,
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Comment"
         }
-    ]
+    ],
+    isIncrease: Boolean,
+    isDecrease: Boolean
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
