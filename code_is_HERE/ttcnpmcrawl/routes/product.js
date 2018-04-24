046613a163f99743aa083d7ce8c24864721d2e19
vu <mainguyenvu210@gmail.com>
2018-04-24 14:44:13 +0700

    edit /tiki

--- code_is_HERE/ttcnpmcrawl/routes/product.js
+++ code_is_HERE/ttcnpmcrawl/routes/product.js
@@ -39,4 +39,40 @@ router.get("/:url_path", function(req, res){
   });
 });
 
+router.get("/delete", function(req, res){
+  Product.remove({}, function(err, done){
+    if(err){
+      console.log("Err delete product " + err);
+    } else {
+      Category.remove({}, function(err, doneTwo){
+        if(err){
+          console.log("Err delete category " + err);
+        } else {
+          console.log("Delete all category and product!");
+          res.redirect("/");
+        }
+      });
+    }
+  });
+});
+
+//search product POST route
+router.post("/search", function(req, res) {
+    // text pre-processing
+    var searchText = (req.body.search).replace(/[^a-zA-Z0-9\s\(\)-]/g,'');
+    Product.find({$text: {$search: searchText}}/*{$or: [{"name": searchText}, {"product_id": searchText}]}*/, function(err, foundProducts){
+      if(err){
+        console.log("Err at /search " + err);
+        res.redirect("/");
+      } else {
+        res.render("found-products", {foundProducts: foundProducts});
+      }
+    })
+    //res.send("Search post route!!!");
+});
+
+router.get("/quen-mat-khau", function(req, res){
+  res.render("forget-password");
+});
+
 module.exports = router;
