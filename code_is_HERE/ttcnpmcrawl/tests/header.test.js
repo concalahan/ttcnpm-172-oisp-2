c52e5e05048afd5dd77b3362c4160fb4532d8fb8
vu <mainguyenvu210@gmail.com>
2018-04-29 15:53:41 +0700

    complete test header

--- code_is_HERE/ttcnpmcrawl/tests/header.test.js
+++ code_is_HERE/ttcnpmcrawl/tests/header.test.js
@@ -15,5 +15,42 @@ afterEach(async () => {
 test('The header has the correct text', async () => {
   const text = await page.getContentsOf('h1');
 
-  expect(text).toEqual('Aztechpro');
+  expect(text).toEqual('Binggo');
+});
+
+test('Have the box for login', async () => {
+  const text = await page.$eval('.account_login', el => el.textContent);
+
+  expect(text).toEqual('Đăng nhập');
+});
+
+test('Have the button for login', async () => {
+  const text = await page.$eval('#btnLogin', el => el.textContent);
+
+  expect(text).toEqual('Đăng nhập ');
+});
+
+test.only('The user can login', async () => {
+  await page.type('#username', 'user@gmail.com');
+  await page.type('#password', 'user');
+  await page.click('#btnLogin');
+
+  // // await page.evaluate(() => {
+  // //   document.querySelector('#btnLogin').click();
+  // // });
+
+  await page.waitFor('#hello');
+  const text = await page.getContentsOf('#hello');
+
+  expect(text).toEqual('Xin chào user@gmail.com');
+});
+
+test('Test search bar with random string that do not match any product', async () => {
+  await page.type('.form-control', 'My Search Putin Musk');
+  await page.click('#search-bar');
+  await page.waitFor('h2');
+
+  const text = await page.getContentsOf('h2');
+  // console.log(text);
+  expect(text).toEqual('Không tìm thấy sản phẩm!');
 });
