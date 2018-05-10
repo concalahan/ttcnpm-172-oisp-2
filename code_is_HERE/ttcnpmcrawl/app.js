e38584a79a7598599729bd2bcb309c3c22d88269
vu <mainguyenvu210@gmail.com>
2018-05-10 19:15:54 +0700

    config

--- code_is_HERE/ttcnpmcrawl/app.js
+++ code_is_HERE/ttcnpmcrawl/app.js
@@ -95,7 +95,14 @@ app.use(function(req, res, next){
 });
 
 // Server setup
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-server.listen(port);
-console.log("Server has started at port ", port);
+if(process.env.NODE_ENV === 'ci'){
+  const port = process.env.PORT || 3000;
+  const server = http.createServer(app);
+  server.listen(port);
+  console.log("Server has started at port ", port);
+} else {
+  const port = process.env.PORT || 80;
+  const server = http.createServer(app);
+  server.listen(port);
+  console.log("Server has started at port ", port);
+}
