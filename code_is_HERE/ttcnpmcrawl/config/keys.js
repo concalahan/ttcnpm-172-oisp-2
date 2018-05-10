bf57ee895d7a6772032057995a58ede16e577c5a
vu <mainguyenvu210@gmail.com>
2018-05-10 16:30:08 +0700

    config

--- code_is_HERE/ttcnpmcrawl/config/keys.js
+++ code_is_HERE/ttcnpmcrawl/config/keys.js
@@ -1,3 +1,7 @@
 if (process.env.NODE_ENV === 'prod') {
   module.exports = require('./prod');
-} 
+} else if(process.env.NODE_ENV === 'dev') {
+  module.exports = require('./dev');
+} else {
+  module.exports = require('./ci');
+}
