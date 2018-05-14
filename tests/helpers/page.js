2466407cecae35b4402fa904a7facd90df9238ca
vu <mainguyenvu210@gmail.com>
2018-05-14 18:30:52 +0700

    config setup puppeteer

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -4,9 +4,11 @@ const userFactory = require('../factories/userFactory');
 
 class CustomPage {
   static async build() {
-    const browser = await puppeteer.launch({
-      headless: false
-    });
+    // const browser = await puppeteer.launch({
+    //   headless: false
+    // });
+
+    const browser = await puppeteer.launch({args: ['--no-sandbox']});
 
     const page = await browser.newPage(); // open new page
     const customPage = new CustomPage(page);
